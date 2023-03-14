5f639a5c78aea34a0035cd8af7c97d51b66b3136
moeota <moe0925c5@gmail.com>
2023-03-14 21:06:54 +0900

    add

--- command.c
+++ command.c
@@ -6,11 +6,12 @@
 /*   By: moeota <moeota@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 19:41:20 by moeota            #+#    #+#             */
-/*   Updated: 2023/03/13 20:06:38 by moeota           ###   ########.fr       */
+/*   Updated: 2023/03/14 21:06:03 by moeota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+void first_put(t_stack *stack_a, t_stack *stack_b);
 
 void sa_command(t_stack **stack_a)
 {
@@ -36,17 +37,40 @@ void pa_command(t_stack **stack_a, t_stack **stack_b)//bの先頭の要素を取
     first_put(*stack_a, *stack_b);
 }
 
-void first_put(t_stack *stack_a, t_stack *stack_b)
+void first_put(t_stack *stack_a, t_stack *stack_b)//bの先頭の要素を取り出し、aの先頭に置く
 {
-    int temp;
-    
+    t_stack *head_a = stack_a;
+    t_stack *head_b = stack_b;
+
     if (!stack_a)
         return ;
-    if (!stack_b)
-    {
-        temp = stack_a->
-    }
+
+    head_b = head_a->next;
+
+    head_b->prev->next = head_b->next;
+    head_b->next->prev = head_b->prev;
+
+    head_a->next->prev = head_b;
+    head_b->next = head_a->next;
+    head_a->next = head_b;
+    head_b->prev = head_b;
+    
     ft_putstr_fd("pa\n",1);
+}
+
+// void	ft_lstadd_back(t_list **lst, t_list *new)
+// {
+// 	if (new == NULL)
+// 		return ;
+// 	while (*lst != NULL)
+// 		lst = &((*lst)-> next);
+// 	*lst = new;
+// }
+
+void ra_command()
+{
+    
+    
 }
 
 
